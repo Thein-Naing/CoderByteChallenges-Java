8abf8126648b1a39b51b6c89945b60a51df889da
Thein Naing <theinnaing0908@gmail.com>
2023-10-31 11:49:53 +0800

    Update PalinDrome.java

--- PalinDrome.java
+++ PalinDrome.java
@@ -19,15 +19,15 @@ class Main {
 
     // code goes here  
 
-public static boolean Palindrome(String str) {           // 1. use boolean 
+public static boolean Palindrome(String str) {           // 1. use boolean for true or false outcome.
 
-     // String.str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();  
-        String str1 = str.replaceAll("\s+", "");                  /* 2. create and use regex ("\s+", "") === remove all white space by replace all  white space with nothing.
-                                                                    The Regexuse str.replaceAll("\s+", "").toLowerCase() OR   
-                                                                    [^a-zA-Z0-9] will delete (replace by "") any characters different from A to Z and 0 till 9.
-                                                                    After that, it will lowercase (.toLowerCase()) all remaining characters in the string, i.e. :
-                                                                    String s = "abc&AA_" will become: abcaa        */
-        StringBuilder str2 = new StringBuilder(str1);    //3. use StringBuilder method to create str2 and reverse. then you need to convert back using toString() method.
+     // String str1 = str.replaceAll("[^a-zA-Z0-9]", ""); 
+        String str1 = str.replaceAll("\s+", "");         /* 2. create and use regex ("\s+", "") === remove all white space by replace all  white space with nothing.
+                                                               Use the Regex: str.replaceAll("\s+", "").toLowerCase() OR another REGEX: str.replaceAll("[^a-zA-Z0-9]", "");
+                                                               The regex matchs all characters which are NOT a-z or A-Z or 0-9. Then these chars are replaced with nothing.                                                                      
+                                                               example: String str = "abc&AA_" will become: abcaa        */
+    
+        StringBuilder str2 = new StringBuilder(str1);    //3. use StringBuilder method to create new str2 and reverse and then convert back using toString() method.
 
         if( str1.equals(str2.reverse().toString())) {    // 4. if str1 == str2, then return true.
              return true;
