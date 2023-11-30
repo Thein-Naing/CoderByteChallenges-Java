c29b57c3557c33cec1da01f068f122dca4765f76
Thein Naing <theinnaing0908@gmail.com>
2023-11-30 13:31:01 +0800

    Update LetterCapitalize.java

--- LetterCapitalize.java
+++ LetterCapitalize.java
@@ -1,3 +1,16 @@
+/* Letter Capitalize
+easy 
+Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. 
+Words will be separated by only one space.
+Examples
+Input: "hello world"
+Output: Hello World
+Input: "i ran there"
+Output: I Ran There
+
+Tags
+string manipulation */
+
 import java.util.*; 
 import java.io.*;
 
@@ -6,12 +19,10 @@ class Main {
   public static String LetterCapitalize(String str) {
     // code goes here  
     String[] words = str.split(" ");
-    StringBuffer result = new StringBuffer();
+    StringBuffer result = new StringBuffer();    // StringBuilder result = new StringBuilder() method also works.
 
-    for ( int i = 0; i < words.length; i++) {
-     result.append(Character.toUpperCase(words[i].charAt(0))).append(words[i].substring(1)).append(" ");
-        
-
+   for (int i = 0; i < words.length; i++) {     
+        result.append(Character.toUpperCase(words[i].charAt(0))).append(words[i].substring(1)).append(" ");
     }
     return result.toString().trim();
   }
